f3aa30a6119bf31ccbb5d3187eaf87a94ec506fc
deityzs <kongkonghihi@gmail.com>
2023-10-25 02:52:46 +0700

    Update outputPage

--- src/views/OutputView.vue
+++ src/views/OutputView.vue
@@ -1,255 +1,49 @@
 <template>
   <section class="relative flex flex-wrap lg:h-screen lg:items-center">
-    <div class="w-full   bg-slate-900 h-full">
+    <div class="w-full bg-slate-900 h-full">
       <div class="p-2 text-start w-full h-full">
       
-      <div class="grid grid-rows-2 grid-cols-3 gap-4">
-        <div class=" bg-white rounded-lg shadow-md dark:bg-gray-800 w-full p-4 ">
+      <div class="grid grid-rows-2 grid-cols-3 gap-2" >
+        <div class=" bg-white rounded-lg shadow-md dark:bg-gray-800 w-full p-4 " v-for="repo in repos" :key="repo.id">
           <div class="flex items-center justify-between">
             <p
               class="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-300 transform bg-gray-600 rounded cursor-pointer hover:bg-gray-500"
               tabindex="0"
             >
-              Name
+              {{ repo.name }}
             </p>
           </div>
 
           <div class="mt-2">
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Lint score :</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test percentage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage grade:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Document score:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Reuseability:</p>
+            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Lint score: {{ repo.lintScore }}</p>
+            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage: {{ repo.testCoverage }}</p>
+            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test percentage grade: {{ repo.testPercentageGrade}}</p>
+            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Document score: {{ repo.documentScore}}</p>
             <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">
-              Link:
-              <a
+              Link: 
+              <a  
                 class="text-xl font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline pl-7"
               >
-               URL LINK
+               {{ repo.url }}
               </a>
             </p>
           </div>
 
           <div class="flex items-center justify-between mt-4">
-            <p class="mt-2 text-gray-600 dark:text-gray-300">Activeness:</p>
+            <p class="mt-2 text-gray-600 dark:text-gray-300">Activeness: {{ repo.activeness }} </p>
 
             <div class="flex items-center">
               <a
                 class="font-bold text-gray-700 cursor-pointer dark:text-gray-200"
                 tabindex="0"
                 role="link"
-                >Grade: "A"
+                >Grade: {{ repo.activenessGrade }}
                 </a>
           </div>
         </div>
       </div>
-
-      <div class="bg-white rounded-lg shadow-md dark:bg-gray-800 w-full p-4">
-          <div class="flex items-start justify-between">
-            <p
-              class="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-300 transform bg-gray-600 rounded cursor-pointer hover:bg-gray-500"
-              tabindex="0"
-            >
-              Name
-            </p>
-          </div>
-
-          <div class="mt-2">
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Lint score :</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test percentage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage grade:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Document score:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Reuseability:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">
-              Link:
-              <a
-                class="text-xl font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline pl-7"
-              >
-               URL LINK
-              </a>
-            </p>
-          </div>
-
-          <div class="flex items-center justify-between mt-4 ">
-            <p class="mt-2 text-gray-600 dark:text-gray-300">Activeness:</p>
-
-            <div class="flex items-center">
-              <p
-                class="font-bold text-gray-700 cursor-pointer dark:text-gray-200"
-                tabindex="0"
-                role="link"
-                >Grade: "A"</p
-              >
-          </div>
-        </div>
-      </div>
-
-      <div class="bg-white rounded-lg shadow-md dark:bg-gray-800 w-full p-4">
-          <div class="flex items-start justify-between">
-            <p
-              class="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-300 transform bg-gray-600 rounded cursor-pointer hover:bg-gray-500"
-              tabindex="0"
-            >
-              Name
-            </p>
-          </div>
-
-          <div class="mt-2">
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Lint score :</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test percentage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage grade:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Document score:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Reuseability:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">
-              Link:
-              <a
-                class="text-xl font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline pl-7"
-              >
-               URL LINK
-              </a>
-            </p>
-          </div>
-
-          <div class="flex items-center justify-between mt-4">
-            <p class="mt-2 text-gray-600 dark:text-gray-300">Activeness:</p>
-
-            <div class="flex items-center">
-              <p
-                class="font-bold text-gray-700 cursor-pointer dark:text-gray-200"
-                tabindex="0"
-                role="link"
-                >Grade: "A"</p
-              >
-          </div>
-        </div>
-      </div>
-
-      <div class="bg-white rounded-lg shadow-md dark:bg-gray-800 w-full p-4">
-          <div class="flex items-start justify-between">
-            <p
-              class="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-300 transform bg-gray-600 rounded cursor-pointer hover:bg-gray-500"
-              tabindex="0"
-            >
-              Name
-            </p>
-          </div>
-
-          <div class="mt-2">
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Lint score :</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test percentage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage grade:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Document score:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Reuseability:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">
-              Link:
-              <a
-                class="text-xl font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline pl-7"
-              >
-               URL LINK
-              </a>
-            </p>
-          </div>
-
-          <div class="flex items-center justify-between mt-4">
-            <p class="mt-2 text-gray-600 dark:text-gray-300">Activeness:</p>
-
-            <div class="flex items-center">
-              <p
-                class="font-bold text-gray-700 cursor-pointer dark:text-gray-200"
-                tabindex="0"
-                role="link"
-                >Grade: "A"</p
-              >
-          </div>
-        </div>
-      </div>
-
-      <div class="bg-white rounded-lg shadow-md dark:bg-gray-800 w-full p-4">
-          <div class="flex items-start justify-between">
-            <p
-              class="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-300 transform bg-gray-600 rounded cursor-pointer hover:bg-gray-500"
-              tabindex="0"
-            >
-              Name
-            </p>
-          </div>
-
-          <div class="mt-2">
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Lint score :</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test percentage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage grade:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Document score:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Reuseability:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">
-              Link:
-              <a
-                class="text-xl font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline pl-7"
-              >
-               URL LINK
-              </a>
-            </p>
-          </div>
-
-          <div class="flex items-center justify-between mt-4">
-            <p class="mt-2 text-gray-600 dark:text-gray-300">Activeness:</p>
-
-            <div class="flex items-center">
-              <p
-                class="font-bold text-gray-700 cursor-pointer dark:text-gray-200"
-                tabindex="0"
-                role="link"
-                >Grade: "A"</p
-              >
-          </div>
-        </div>
-      </div>
-
-      <div class="bg-white rounded-lg shadow-md dark:bg-gray-800 w-full p-4">
-          <div class="flex items-start justify-between">
-            <p
-              class="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-300 transform bg-gray-600 rounded cursor-pointer hover:bg-gray-500"
-              tabindex="0"
-            >
-              Name
-            </p>
-          </div>
-
-          <div class="mt-2">
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Lint score :</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test percentage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Test coverage grade:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Document score:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">Reuseability:</p>
-            <p class="mt-2 text-gray-600 dark:text-gray-300 pl-7">
-              Link:
-              <a
-                class="text-xl font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline pl-7"
-              >
-               URL LINK
-              </a>
-            </p>
-          </div>
-
-          <div class="flex items-center justify-between mt-4 ">
-            <p class="mt-2 text-gray-600 dark:text-gray-300">Activeness:</p>
-
-            <div class="flex items-center ">
-              <p
-                class="font-bold text-gray-700 cursor-pointer dark:text-gray-200"
-                tabindex="0"
-                role="link"
-                >Grade: "A"</p
-              >
-          </div>
-        </div>
-      </div>
     </div>
+    
 
       </div>
     </div>
@@ -258,4 +52,13 @@
   </section>
 </template>
 
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { RepoItem } from '@/type'
+import { useRepoStore } from '@/stores/libstore';
+import { storeToRefs } from 'pinia';
+const repos = storeToRefs(useRepoStore()).repo
+
+
+</script>
