1a3758cb3062bf50f4ed54bd61b65cd159616937
GIgaSora <krittayoch_jo@cmu.ac.th>
2023-10-24 23:59:16 +0700

    Main page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,23 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-    color: {
-      ransparent: 'transparent',
-      current: 'currentColor',
-      'Kantor-blue': '#062258',
-      'Ultramarine': '#0D3179',
-      'Macrage-blue': '#1946A0',
-      'Lothern-blue': '3D61A7',
-      'Blue-Horror' : '#6B88C2',
-      'SPDark-blue' : '#17181A',
-      'SPLight-blue': '#B8CAED'
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'Kantor-blue': '#062258',
+        'Ultramarine': '#0D3179',
+        'Macrage-blue': '#1946A0',
+        'Lothern-blue': '#3D61A7',
+        'Blue-Horror': '#6B88C2',
+        'SPDark-blue': '#17181A',
+        'SPLight-blue': '#B8CAED',
+        'OrangeIsIt?': '#d97706',
+        'Yellow?': '#F8CD00'
+      }
     }
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+  plugins: [require('@tailwindcss/forms')]
 } satisfies Config
-
