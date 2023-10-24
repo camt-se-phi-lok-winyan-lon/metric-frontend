a5c1d164589f780be97dd5300f441241eb0e3d20
KAN KATPARK <kant.cmd@gmail.com>
2023-10-24 22:51:12 +0700

    update tailwind setup

---
+++ src/views/MainPageView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <h1 class="text-amber-500">Hello Student</h1>
+  <RouterLink :to="{name: 'input-page'}"> Home</RouterLink>
+
+  <RouterLink :to="{name: 'input-page'}"> Result</RouterLink>
+
+  <RouterView/>
+</template>
+
+<style scoped>
+
+</style>
