b270475b04db70011a13648a5bab1bf6924d8052
GIgaSora <krittayoch_jo@cmu.ac.th>
2023-10-25 00:21:22 +0700

    fix main

--- src/views/MainPageView.vue
+++ src/views/MainPageView.vue
@@ -4,21 +4,22 @@
   <section class="relative flex flex-wrap lg:h-screen lg:items-center">
     <div class="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24 bg-slate-900 h-full">
       <div class="mx-auto max-w-lg text-center">
-        <h1 class="text-2xl font-bold sm:text-3xl text-white">Get started <span class="text-Macrage-blue">Here!</span></h1>
+        <h1 class="text-2xl font-bold sm:text-3xl text-white">さぁぁぁぁ <span class="text-Macrage-blue">始めましょう!</span>
+        </h1>
 
         <p class="mt-4 text-Blue-Horror">
-          Select your Repo here
+          リポジトリの選択
         </p>
       </div>
 
       <div class="mb-10">
         <label for="HeadlineAct" class="block text-sm font-medium text-Yellow?">
-        First 🗿
+          一 🗿
         </label>
 
         <select name="HeadlineAct" id="HeadlineAct"
           class="mt-1.5 w-full rounded-lg border-Macrage-blue text-gray-700 bg-slate-400 sm:text-sm">
-          <option value="">Don't select me 🤨</option>
+          <option value="">私を選ばないでください 🤨</option>
           <option value="JM">John Mayer</option>
           <option value="SRV">Stevie Ray Vaughn</option>
           <option value="JH">Jimi Hendrix</option>
@@ -27,17 +28,17 @@
           <option value="BG">Buddy Guy</option>
           <option value="EC">Eric Clapton</option>
         </select>
-        
+
       </div>
 
       <div class="mb-10">
         <label for="HeadlineAct" class="block text-sm font-medium text-Yellow?">
-        Segg 🗿
+          二 🗿
         </label>
 
         <select name="HeadlineAct" id="HeadlineAct"
           class="mt-1.5 w-full rounded-lg border-Macrage-blue text-gray-700 bg-slate-400 sm:text-sm">
-          <option value="">Don't select me 🤨</option>
+          <option value="">私を選ばないでください 🤨</option>
           <option value="JM">John Mayer</option>
           <option value="SRV">Stevie Ray Vaughn</option>
           <option value="JH">Jimi Hendrix</option>
@@ -46,17 +47,17 @@
           <option value="BG">Buddy Guy</option>
           <option value="EC">Eric Clapton</option>
         </select>
-        
+
       </div>
 
       <div class="mb-10">
         <label for="HeadlineAct" class="block text-sm font-medium text-Yellow?">
-        Third 🗿
+          三 🗿
         </label>
 
         <select name="HeadlineAct" id="HeadlineAct"
           class="mt-1.5 w-full rounded-lg border-Macrage-blue text-gray-700 bg-slate-400 sm:text-sm">
-          <option value="">Don't select me 🤨</option>
+          <option value="">私を選ばないでください 🤨</option>
           <option value="JM">John Mayer</option>
           <option value="SRV">Stevie Ray Vaughn</option>
           <option value="JH">Jimi Hendrix</option>
@@ -65,17 +66,17 @@
           <option value="BG">Buddy Guy</option>
           <option value="EC">Eric Clapton</option>
         </select>
-        
+
       </div>
 
       <div class="mb-10">
         <label for="HeadlineAct" class="block text-sm font-medium text-Yellow?">
-        FOUR!!! 🗿
+          四 🗿
         </label>
 
         <select name="HeadlineAct" id="HeadlineAct"
           class="mt-1.5 w-full rounded-lg border-Macrage-blue text-gray-700 bg-slate-400 sm:text-sm">
-          <option value="">Don't select me 🤨</option>
+          <option value="">私を選ばないでください 🤨</option>
           <option value="JM">John Mayer</option>
           <option value="SRV">Stevie Ray Vaughn</option>
           <option value="JH">Jimi Hendrix</option>
@@ -84,17 +85,17 @@
           <option value="BG">Buddy Guy</option>
           <option value="EC">Eric Clapton</option>
         </select>
-        
+
       </div>
 
       <div class="mb-10">
         <label for="HeadlineAct" class="block text-sm font-medium text-Yellow?">
-        five 🗿
+          五 🗿
         </label>
 
         <select name="HeadlineAct" id="HeadlineAct"
           class="mt-1.5 w-full rounded-lg border-Macrage-blue text-gray-700 bg-slate-400 sm:text-sm">
-          <option value="">Don't select me 🤨</option>
+          <option value="">D私を選ばないでください 🤨</option>
           <option value="JM">John Mayer</option>
           <option value="SRV">Stevie Ray Vaughn</option>
           <option value="JH">Jimi Hendrix</option>
@@ -103,17 +104,22 @@
           <option value="BG">Buddy Guy</option>
           <option value="EC">Eric Clapton</option>
         </select>
-        
+
       </div>
 
-      <button type="submit" class="inline-block rounded-lg bg-OrangeIsIt? px-5 py-3 text-sm font-medium text-white">
-          Summit
-        </button>
-      </div>
+      <!-- Button -->
 
-        
-    
-    
+      <a class="inline-block rounded border border-OrangeIsIt? bg-OrangeIsIt? px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-Macrage-blue focus:outline-none focus:ring active:text-indigo-500"
+        href="/download">
+        提出する
+      </a>
+
+
+    </div>
+
+
+
+
 
     <div class="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
       <img alt="Welcome" src="https://media.tenor.com/VFFJ8Ei3C2IAAAAC/rickroll-rick.gif"
